e6467935bdde3c2e9b2eaaa64dd8718332ad0765
Katie Maxwell <katiemaxwell10@hotmail.com>
2017-07-26 15:59:59 +0100

    settings

--- kgmaxwellportfolio/settings.py
+++ kgmaxwellportfolio/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['katie-dev-portfolio.herokuapp.com', '127.0.0.1']
+INTERNAL_IPS = ['127.0.0.1']
 
 
 # Application definition
