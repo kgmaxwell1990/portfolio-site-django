f60274232cd9956f9fd90c62785cdd04ed5acf6d
Katie Maxwell <katiemaxwell10@hotmail.com>
2017-08-04 14:22:53 +0100

    email settings

--- kgmaxwellportfolio/settings.py
+++ kgmaxwellportfolio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-from .email_info import *
+from home.email_credentials import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
