80891b367b8a6b82ea4684c67b6d833b15d744a6
Katie Maxwell <katiemaxwell10@hotmail.com>
2018-02-01 16:10:28 +0000

    animated text using typewriter.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,53 @@
 $(document).ready(function(){
+
+    $('#hi').addClass('animated fadeIn');
+    $('#i_am').addClass('animated fadeInRight')
+    $('#view').addClass('animated fadeIn');
+
+    var app = document.getElementById('app');
+    var app2 = document.getElementById('app2');
+    
+    var typewriter1 = new Typewriter(app, {
+        loop: false,
+        cursor: "",
+        typingSpeed: 30
+        
+    });
+    var typewriter2 = new Typewriter(app2, {
+      loop: true,
+      cursor: "",
+      deleteSpeed: 30,
+      typingSpeed: 30
+  });
+    
+    typewriter1.pauseFor(3000)
+        .typeString('I am a Full Stack Developer')
+        .start();
+
+    typewriter2.pauseFor(5000)
+      .typeString('I code using ')
+      .typeString('HTML')
+      .pauseFor(1500)
+      .deleteChars(4)
+      .typeString('CSS')
+      .pauseFor(1500)
+      .deleteChars(3)
+      .typeString('JavaScript')
+      .pauseFor(1500)
+      .deleteChars(10)
+      .typeString('Python')
+      .pauseFor(1500)
+      .start()
+      
+      
+
+       
+
     $("nav").find("li").on("click", "a", function () {
         $('.navbar-collapse.in').collapse('hide');
     });
-    // $("#titleline").fadeIn(1000);
+
+    
 
     $(window).scroll(function() { // check if scroll event happened
       if ($(document).scrollTop() > 150) { // check if user scrolled more than 50 from top of the browser window
